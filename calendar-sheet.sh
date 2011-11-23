b6e5ce41db051a865c18dec39c8c5f944134dbec
Jonathan Hernández <ion@suavizado.com>
2011-11-22 21:54:49 -0700

    Optimize

--- calendar-sheet.sh
+++ calendar-sheet.sh
@@ -1,12 +1,5 @@
-# Do months
-echo "Creating months..."
-for m in JAN FEB MAR APR MAY JUN JUL AGO SEP OCT NOV DEC
-do
-  convert -background none -fill white -font Helvetica-Bold -pointsize 8 label:$m tmp/$m.png
-done
-
+echo "Generating calendar days..."
 # Do days
-echo "Creating days..."
 for d in {1..31}
 do
   convert -background none -fill black -font Helvetica-Bold -pointsize 14 label:$d tmp/$d.png
@@ -15,10 +8,11 @@ done
 # Do 365 days
 for m in JAN FEB MAR APR MAY JUN JUL AGO SEP OCT NOV DEC
 do
+  convert -background none -fill white -font Helvetica-Bold -pointsize 8 label:$m tmp/tmp1.png
+  composite -gravity center -geometry +0-7 tmp/tmp1.png bg.png tmp/tmp2.png
   for d in {1..31}
   do
-    echo "Creating $m $d image..."
-    composite -gravity center -geometry +0-7 tmp/$m.png bg.png tmp/tmp.png
-    composite -gravity center -geometry +0+5 tmp/$d.png tmp/tmp.png output/$m$d.png
+    composite -gravity center -geometry +0+5 tmp/$d.png tmp/tmp2.png output/$m$d.png
   done
 done
+echo "Done."
